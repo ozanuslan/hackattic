7923b855cfdba107d6cda5a02db9f8a3043ca236
Ozan Uslan <github@ozanuslan.com>
2024-04-16 10:12:17 +0300

    feat(challenges/backup_restore): improve runner script

--- challenges/backup_restore/run.sh
+++ challenges/backup_restore/run.sh
@@ -1,12 +1,19 @@
-#! /bin/bash
+#! /usr/bin/env bash
+
+# This script is used to run the backup_restore challenge.
+# You can set the CLEAN_BUILD environment variable to force a clean image build.
 
 set -euo pipefail
 
 img_name="hackattic_backup_restore"
 
-# if the image does not exist, build it
-if ! docker image inspect "$img_name" &>/dev/null; then
+if ! command -v docker &>/dev/null; then
+    echo "docker command not found" >&2
+    exit 1
+fi
+
+if [[ -z "$(docker images -q "$img_name")" || -n "${CLEAN_BUILD:-}" ]]; then
     docker build -t "$img_name" .
 fi
 
-docker run -i --rm "$img_name" </dev/stdin
+docker run -i --rm "$img_name" 2> >(while read line; do echo "[container] $line" >&2; done) </dev/stdin
