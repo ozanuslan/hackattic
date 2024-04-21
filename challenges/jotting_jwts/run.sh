439c229432bf6fc20893e838cbc9e06d31c5f324
Ozan Uslan <github@ozanuslan.com>
2024-04-21 22:13:16 +0300

    style(challenges/jotting_jwts): line length

--- challenges/jotting_jwts/run.sh
+++ challenges/jotting_jwts/run.sh
@@ -10,7 +10,12 @@ fi
 self_dir=$(dirname "$0")
 source "$self_dir/common.sh"
 
-docker run -d --net=host --rm --name "$ngrok_container_name" -e NGROK_AUTHTOKEN="$NGROK_AUTH_TOKEN" "$ngrok_img_name" http --domain="$NGROK_DOMAIN" 1337 >/dev/null
+docker run -d \
+    --rm \
+    --net=host \
+    --name "$ngrok_container_name" \
+    -e NGROK_AUTHTOKEN="$NGROK_AUTH_TOKEN" \
+    "$ngrok_img_name" http --domain="$NGROK_DOMAIN" 1337 >/dev/null
 
 if [[ -z "$(docker images -q "$img_name")" || -n "${CLEAN_BUILD:-}" ]]; then
     docker build -t "$img_name" . >&2
@@ -19,6 +24,10 @@ fi
 echo -n "{\"app_url\":\"https://$NGROK_DOMAIN\"}"
 
 exec $(
-    docker run -i -p 1337:1337 --rm --name "$container_name" "$img_name" </dev/stdin >/dev/null 2> >(while read -r line; do echo "[container] $line" >&2; done) &
+    docker run -i \
+        --rm \
+        -p 1337:1337 \
+        --name "$container_name" \
+        "$img_name" </dev/stdin >/dev/null 2> >(while read -r line; do echo "[container] $line" >&2; done) &
     sleep 1
 )
