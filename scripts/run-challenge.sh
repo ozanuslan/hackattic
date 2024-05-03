32542647b4d403480039e6c4d28cce4f7b7df324
Ozan Uslan <github@ozanuslan.com>
2024-05-03 21:01:13 +0300

    docs(scripts/run-challenge): add clean-build info

--- scripts/run-challenge.sh
+++ scripts/run-challenge.sh
@@ -4,6 +4,7 @@
 # It can also verify the challenge output by running the verify-challenge.sh script.
 # If you want to verify the challenge after you've passed, you can use the playground option.
 # If you want to get new input for the challenge, you can use the new-input option.
+# If the challenge has a container, you can clean build it by using the clean-build option.
 
 set -euo pipefail
 
